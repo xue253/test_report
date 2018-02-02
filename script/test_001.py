4115ef17e5ec9eb9361d11d68e89549ad6cb8a81
xue466088253 <xue466088253@163.com>
2018-02-02 17:21:56 +0800

    123

--- script/test_001.py
+++ script/test_001.py
@@ -2,6 +2,5 @@ import pytest,allure
 class Test_A():
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step("第一条测试用例")
-    @allure.attach("描述","判断正确")
     def test_001(self):
         assert 1
